b11de2d56a14156547e354f17ff69127b6d8315a
WithLoveIn <66476913+WithLoveIn@users.noreply.github.com>
2020-06-10 09:29:06 +0300

    add
    +

--- webshop/with_love_in(main).js
+++ webshop/with_love_in(main).js
@@ -1,14 +1,27 @@
 var openaside=0;
 function asideopen(){
 	var as=document.getElementById('main_bar');
-	if (openaside==0)
-	{
+	if (openaside==0){
 		as.style.display='flex';
 		openaside=1;
-	}
-	else
-	{
+	}else{
 		as.style.display='none';
 		openaside=0;
 	}
 }
+var f=function(){
+	var eventHandler =function (event){
+		var as=document.getElementById('main_bar');
+		if (window.innerWidth>767){
+			openaside=1;
+			as.style.display='flex';
+		}
+		else{
+			as.style.display='none';
+			openaside=0;
+		}
+	}
+	window.addEventListener('resize',eventHandler, false);
+};
+
+document.addEventListener('DOMContentLoaded',f,false);
